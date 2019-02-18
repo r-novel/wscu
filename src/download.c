b382d1fc8c5e866143566805ca8aa3cc11306aec
Novel Corpse <romandafe94@gmail.com>
2019-02-18 23:39:54 +0300

    refs #20; tiny fixes function for ignore curl output;

--- src/download.c
+++ src/download.c
@@ -10,9 +10,9 @@ size_t tool_fwrite(void *buf, size_t size, size_t nmemb, void* stream) {
 	return fwrite(buf, size, nmemb, out->stream);
 }
 
-size_t ignore_curl_output( void *ptr, size_t size, size_t nmemb, void *stream) {
-	(void) ptr;
-  (void) stream;
+static size_t ignore_curl_output( void* ptr, size_t size, size_t nmemb, void* stream) {
+	(void)ptr;
+  (void)stream;
   return size * nmemb;
 }
 
