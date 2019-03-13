a689b93ee8b633db777df5c7e1281de640a90a2b
Novel Corpse <romandafe94@gmail.com>
2019-03-13 23:55:53 +0300

    refs #26; added new lines into files;

--- include/const.h
+++ include/const.h
@@ -6,4 +6,5 @@
 #define DEFAULT_VIM_URL "https://github.com/vim/vim/archive/v8.1.0788.tar.gz"
 #define DEFAULT_CONFIG_PATH "./config/config_example.yaml"
 #define TEST_NAME "tmux-2.8/configure"
+
 #endif // _CONST_H_
